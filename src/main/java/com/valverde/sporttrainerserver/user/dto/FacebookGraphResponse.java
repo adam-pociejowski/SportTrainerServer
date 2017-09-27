fd49d66c174c758bfdbac9dc1c3302091d5bbd9c
adam-pociejowski <valverde12345@gmail.com>
2017-09-27 19:20:21 +0200

    Added default constructor to FacebookGraphResponse needed to Jackson

--- src/main/java/com/valverde/sporttrainerserver/user/dto/FacebookGraphResponse.java
+++ src/main/java/com/valverde/sporttrainerserver/user/dto/FacebookGraphResponse.java
@@ -2,11 +2,16 @@ package com.valverde.sporttrainerserver.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class FacebookGraphResponse {
+
     private String name;
+
     private String email;
+
     private String id;
 }
